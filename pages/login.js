ca8e7a6f04158cbb535158e8af267e054b0bcdb1
LeMasters <jacksonlemasters@outlook.com>
2022-02-14 15:15:45 -0700

    worked on nextauth and login page

--- pages/login.js
+++ pages/login.js
@@ -7,8 +7,9 @@ function Login({ providers }) {
             <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="Spotify logo" />
 
             {Object.values(providers).map((provider) => {
-                <div><button>
-                    test
+                <div key={provider.name}>
+                    <button className="bg-[#18D860] text-white p-5 rounded-lg">
+                    Login with {provider.name}
                     </button></div>
             })}
         </div>
