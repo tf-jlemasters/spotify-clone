fcec6c1571e6c109a3cd7c2911f85c9beae1cacf
Jack LeMasters <jacksonlemasters@outlook.com>
2022-02-26 19:09:07 -0700

    fixed css typo on profile icon

--- components/Center.js
+++ components/Center.js
@@ -38,7 +38,7 @@ function Center() {
     return (
         <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
             <header className="absolute top-5 right-8">
-                <div className="flex item-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white" onClick={signOut}> 
+                <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white" onClick={signOut}> 
                     <img 
                     className="rounded-full w-10 h-10" 
                     src={session?.user.image} 
